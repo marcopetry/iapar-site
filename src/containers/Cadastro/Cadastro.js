f4c135e13531e31394c48f694e7ab99958e44fe8
marcopetry <marcomattospetry@gmail.com>
2020-01-23 13:11:32 -0300

    Associado os inputs as variáveis e criado a api com axios.

--- src/containers/Cadastro/Cadastro.js
+++ src/containers/Cadastro/Cadastro.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import logo from '../../assets/logo-iapar.png';
 import './Cadastro.css';
 import { Select, Grid, FormControl, InputLabel, Input, FormGroup, Container, MenuItem, Button } from '@material-ui/core';
+import { Link } from 'react-router-dom';
 
 export default function Cadastro() {
     const [nome, setNome] = useState('');
@@ -24,68 +25,132 @@ export default function Cadastro() {
             <Container maxWidth="sm" className="container-form-cadastro">
                 <FormControl margin="dense" fullWidth="true">
                     <InputLabel htmlFor="nome-completo">Nome completo</InputLabel>
-                    <Input id="nome-completo" aria-describedby="my-helper-text" />
+                    <Input id="nome-completo" 
+                        aria-describedby="my-helper-text" 
+                        required
+                        onChange={(e) => setNome(e.target.value)}
+                        value={nome}
+                    />
                 </FormControl>
                 <FormGroup row="true">
                     <FormControl margin="dense" className="input-lg">
                         <InputLabel htmlFor="cpf">CPF</InputLabel>
-                        <Input id="cpf" aria-describedby="my-helper-text" />
+                        <Input id="cpf" 
+                            aria-describedby="my-helper-text"
+                            required
+                            onChange={(e) => setCPF(e.target.value)}
+                            value={cpf}
+                        />
                     </FormControl>
                     <FormControl margin="dense" className="input-md">
                         <InputLabel htmlFor="telefone">Telefone</InputLabel>
-                        <Input id="telefone" aria-describedby="my-helper-text" />
+                        <Input id="telefone" 
+                            aria-describedby="my-helper-text" 
+                            required
+                            onChange={(e) => setTelefone(e.target.value)}
+                            value={telefone}
+                        />
                     </FormControl>
                 </FormGroup>
                 <FormGroup row="true">
                     <FormControl margin="dense" className="input-lg">
                         <InputLabel htmlFor="cidade">Cidade</InputLabel>
-                        <Input id="cidade" aria-describedby="my-helper-text" />
+                        <Input id="cidade" 
+                            aria-describedby="my-helper-text" 
+                            required
+                            onChange={(e) => setCidade(e.target.value)}
+                            value={cidade}
+                        />
                     </FormControl>
                     <FormControl margin="dense" className="input-md">
                         <InputLabel htmlFor="cep" >Cep</InputLabel>
-                        <Input id="cep" aria-describedby="my-helper-text" />
+                        <Input id="cep" 
+                            aria-describedby="my-helper-text" 
+                            required
+                            onChange={(e) => setCep(e.target.value)}
+                            value={cep}
+                        />
                     </FormControl>
                 </FormGroup>
                 <FormControl margin="dense" fullWidth="true">
                     <InputLabel htmlFor="rua">Rua</InputLabel>
-                    <Input id="rua" aria-describedby="my-helper-text" />
+                    <Input id="rua" 
+                        aria-describedby="my-helper-text" 
+                        required
+                        onChange={(e) => setRua(e.target.value)}
+                        value={rua}
+                    />
                 </FormControl>
                 <FormGroup row="true">
                     <FormControl margin="dense" className="input-lg">
                         <InputLabel htmlFor="bairro">Bairro</InputLabel>
-                        <Input id="bairro" aria-describedby="my-helper-text" />
+                        <Input id="bairro" 
+                            aria-describedby="my-helper-text" 
+                            required
+                            onChange={e => setBairro(e.target.value)}
+                            value={bairro}
+                        />
                     </FormControl>
                     <FormControl margin="dense" className="input-md">
                         <InputLabel htmlFor="numero" >Número</InputLabel>
-                        <Input id="numero" aria-describedby="my-helper-text" />
+                        <Input id="numero" 
+                            aria-describedby="my-helper-text" 
+                            required
+                            onChange={(e) => setNumero(e.target.value)}
+                            value={numero}
+                        />
                     </FormControl>
                 </FormGroup>
                 <FormControl margin="dense" fullWidth="true">
                     <InputLabel htmlFor="email">Email</InputLabel>
-                    <Input id="email" aria-describedby="my-helper-text" />
+                    <Input id="email" 
+                        aria-describedby="my-helper-text" 
+                        required
+                        onChange={(e) => setEmail(e.target.value)}
+                        value={email}
+                    />
                 </FormControl>
                 <FormControl margin="dense" fullWidth="true">
                     <InputLabel htmlFor="senha">Senha</InputLabel>
-                    <Input id="senha" aria-describedby="my-helper-text" />
+                    <Input id="senha" 
+                        aria-describedby="my-helper-text" 
+                        required
+                        onChange={(e) => setSenha(e.target.value)}
+                        value={senha}
+                    />
                 </FormControl>
                 <FormGroup row="true">
                     <FormControl margin="dense" className="input-md">
-                        <InputLabel htmlFor="numero-registro" >Número do Registro</InputLabel>
-                        <Input id="numero-registro" aria-describedby="my-helper-text" />
-                    </FormControl>
-                    <FormControl margin="dense" className="input-md ml-input-md">
                         <InputLabel htmlFor="tipo-registro" >Tipo Registro</InputLabel>
-                        <Select id="tipo-registro" aria-describedby="my-helper-text">
+                        <Select id="tipo-registro" 
+                            aria-describedby="my-helper-text"
+                            onChange={e => setTipoRegistro(e.target.value)}
+                            value={tipoRegistro}
+                        >
                             <MenuItem value="CREA">CREA</MenuItem>
                             <MenuItem value="CRMV">CRMV</MenuItem>
                         </Select>
                     </FormControl>
                     <FormControl margin="dense" className="input-md ml-input-md">
+                        <InputLabel htmlFor="numero-registro" >Número do Registro</InputLabel>
+                        <Input id="numero-registro" 
+                            aria-describedby="my-helper-text" 
+                            required
+                            onChange={e => setRegistro(e.target.value)}
+                            value={registro}
+                        />
+                    </FormControl>
+                    <FormControl margin="dense" className="input-md ml-input-md">
                         <InputLabel htmlFor="ano-formatura" >Ano Formatura</InputLabel>
-                        <Input id="ano-formatura" aria-describedby="my-helper-text" />
+                        <Input id="ano-formatura" 
+                            aria-describedby="my-helper-text" 
+                            required
+                            onChange={e => setAnoFormatura(e.target.value)}
+                            value={anoFormatura}
+                        />
                     </FormControl>
                 </FormGroup>
-                <Button variant="contained" fullWidth="true" className="btn-form" onClick={() => alert('clicou')}>
+                <Button variant="contained" fullWidth="true" className="btn-form" onClick={() => alert(tipoRegistro)}>
                     Cadastrar
                 </Button>
             </Container>
